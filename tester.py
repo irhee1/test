227cbf73a8c0ead55decac438763c08bdb89cfeb
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-13 11:57:24 -0800

    tester.py

--- tester.py
+++ tester.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.set_page_config(page_title="My first App", page_icon="🐼", layout="wide")
-st.write("This is a test website!")
+st.header("This is a test website!", anchor="test")
