ded53f919ff1432668270e79775de4105e2b9c7f
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-18 21:18:01 -0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,4 +1,8 @@
 import streamlit as st
-st.set_page_config(page_title="My first App", page_icon="🐼", layout="wide")
-st.header("This is a test website!", anchor="test")
-"hi"
+from streamlit_lottie import st_lottie
+st.set_page_config(page_title="All about me!", page_icon="🐼", layout="wide")
+st.header("All about me")
+st.caption("This is my fitst website posted on streamlit!")
+l, m, r = st.columns(3)
+lottie_robots = l.json("https://lottie.host/b80df535-7d0b-4eeb-81fa-1b5362f0b651/OMFm2v3X3V.json")
+st_lottie(lottie_robots,height="300", key="robots")
