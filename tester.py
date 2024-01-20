38bff947786c09dbf9863f37f335a81b3005775a
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-19 16:40:00 -0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -11,14 +11,15 @@ rain(
 st.header("All about me")
 st.caption("This is my first website posted on streamlit cloud!")
 st.write("---")
-l, r = st.columns(2)
-l_ex = l.expander("Stuff about me!")
-r_ex = r.expander("More")
-with l_ex:
-  l_ex.write("I'm a kid doing coding stuff normally in python. I can make simple websites using streamlit (it's really easy to use). \nP.S: I might make more websites")
-with r_ex:
-  r_ex.header("My channel")
-  r_ex.link_button("The channel", url="https://www.youtube.com/channel/UCU2ciboF3zbv9sV4Bir18NQ")
-  r_ex.header("My other streamlit websites (will be finished by Jan 10, 2025)")
+l, m, r = st.tabs(["Home", "Me", "Website"])
+with l:
+  st.header("Home page", anchor="Home")
+with m:
+  st.header("All about me", anchor="Me")
+  st.write("I'm a kid named Isaac Rhee doing coding stuff normally in python. I can make simple websites using streamlit (it's really easy to use). \nP.S: I might make more websites")
+with r:
+  st.header("My channel", anchor="Website")
+  st.link_button("The channel", url="https://www.youtube.com/channel/UCU2ciboF3zbv9sV4Bir18NQ")
+  st.header("My other streamlit websites (will be finished by Jan 10, 2025)")
   badge(type="streamlit", url="https://python-programs.streamlit.app")
   badge(type="streamlit", url="https://pygame-programs.streamlit.app")
