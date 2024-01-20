a915826ca8486191e32ad50f3cbcad1ee1ef85c0
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-19 16:07:09 -0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -11,18 +11,14 @@ rain(
 st.header("All about me")
 st.caption("This is my first website posted on streamlit cloud!")
 st.write("---")
-l, m, r = st.columns(3)
+l, r = st.columns(2)
 l_ex = l.expander("Stuff about me!")
-m_ex = m.expander("What I like doing!")
 r_ex = r.expander("More")
 with l_ex:
-  pass
-with m_ex:
-  m_ex.subheader("Coding!")
-  m_ex.subheader("Building robots")
-  m_ex.subheader("Math")
+  l_ex.write("I'm a kid doing coding stuff normally in python. I can make simple websites using streamlit (it's really easy to use). \nP.S: I might make more websites")
 with r_ex:
   r_ex.header("My channel")
+  r.ex.link_button("The channel", url="https://www.youtube.com/channel/UCU2ciboF3zbv9sV4Bir18NQ")
   r_ex.header("My other streamlit websites (will be finished by Jan 10, 2025)")
   badge(type="streamlit", url="https://python-programs.streamlit.app")
   badge(type="streamlit", url="https://pygame-programs.streamlit.app")
