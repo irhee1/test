3ddbb268b8896c313271763c2013b53900ecd52a
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-19 16:07:39 -0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -18,7 +18,7 @@ with l_ex:
   l_ex.write("I'm a kid doing coding stuff normally in python. I can make simple websites using streamlit (it's really easy to use). \nP.S: I might make more websites")
 with r_ex:
   r_ex.header("My channel")
-  r.ex.link_button("The channel", url="https://www.youtube.com/channel/UCU2ciboF3zbv9sV4Bir18NQ")
+  r_ex.link_button("The channel", url="https://www.youtube.com/channel/UCU2ciboF3zbv9sV4Bir18NQ")
   r_ex.header("My other streamlit websites (will be finished by Jan 10, 2025)")
   badge(type="streamlit", url="https://python-programs.streamlit.app")
   badge(type="streamlit", url="https://pygame-programs.streamlit.app")
